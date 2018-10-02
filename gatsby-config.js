2691e05618590bde680e2eeecf5b991c3d4eda73
Steven Heffner <stevenheffner@Stevens-MacBook-Pro-2.local>
2018-10-02 15:52:01 -0600

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Ufa Concept',
+  },
+  pathPrefix: "/",
+  plugins: [
+    {
+      resolve: "gatsby-source-prismic",
+      options: {
+        repositoryName: "ufa-test-2",
+        linkResolver: ({ node, key, value }) => doc => {
+          return "/";
+        },
+        htmlSerializer: ({ node, key, value }) => (
+          type,
+          element,
+          content,
+          children
+        ) => {}
+      }
+    },
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-offline',
+  ],
+}

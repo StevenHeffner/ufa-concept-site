3b966d243508d54919e3621cf37342f9e7bbf857
Steven Heffner <stevenheffner@Stevens-MacBook-Pro-2.local>
2018-10-09 10:58:40 -0600

    nav restructure done

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,14 +6,33 @@ exports.createPages = async ({ graphql, boundActionCreators }) => {
     'page': path.resolve('./src/templates/customPage.jsx')
   }
 
-  const pages = await graphql(`
-  {
-    allPrismicPage {
+  const navData = await graphql(`
+  query {
+    allPrismicMenu {
       edges {
         node {
-          uid
-          prismicId
-          type
+          data {
+            body {
+              items {
+                link_to_menu_item {
+                  document {
+                    data {
+                      first_level_text
+                      body {
+                        primary {
+                          link_text
+                        }
+                        items {
+                          _3rd_level_link_text
+                          third_level_uid
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
         }
       }
     }
@@ -21,15 +40,34 @@ exports.createPages = async ({ graphql, boundActionCreators }) => {
   `)
 
 
-  pages.data.allPrismicPage.edges.forEach((edge) => {
-    let url = edge.node.uid.split('_').join('/')
+  // pages.data.allPrismicPage.edges.forEach((edge) => {
+  //   let url = edge.node.uid.split('_').join('/')
+  //   createPage({
+  //     path: url,
+  //     component: pageTemplates[edge.node.type],
+  //     context: {
+  //       uid: edge.node.uid
+  //     }
+  //   })
+  // })
+  
+  navData.data.allPrismicMenu.edges[0].node.data.body[0].items.forEach(item => {
+    let firstLevelLabel = item.link_to_menu_item.document[0].data.first_level_text
+    let secondLevelLabel = item.link_to_menu_item.document[0].data.body[0].primary.link_text
     createPage({
-      path: url,
-      component: pageTemplates[edge.node.type],
-      context: {
-        uid: edge.node.uid
-      }
+      path: `/${firstLevelLabel}/${secondLevelLabel}/:id`,
+      //////THIS WILL NEED TO CHANGE//////////////////////
+      component: path.resolve('./src/templates/customPage.jsx')
+      })
+    item.link_to_menu_item.document[0].data.body[0].items.forEach(item => {
+    //   let thirdLevelLabel = item.third_level_uid
+    //   createPage({
+    //     path: `/${firstLevelLabel}/${secondLevelLabel}/${thirdLevelLabel}`,
+    //     component: 
+    //   })
     })
+    
   })
+
 
 }
--- src/components/Header/components/SubMenuItem.js
+++ src/components/Header/components/SubMenuItem.js
@@ -6,21 +6,20 @@ import { Link } from 'gatsby'
 const Wrapper = styled.div`
   width: 100%;
   display: flex;
-  flex-direction: collum;
+  flex-direction: column;
   align-items: center;
 `
 
 const SecondLevelLink = styled.div`
-  height: 40px;
   width: 100%;
   background: #ffffff;
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column
   font-size: 12px;
-  cursor: pointer;
   transition: background .2s linear
-  color: #9F090B;
+  color: black;
 
   &:hover {
     background: #f3f3f3;
@@ -32,18 +31,38 @@ const SecondLevelLink = styled.div`
         ? `0px 0px 5px 5px`
         : null};
 `
+const ThirdLevelLink = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  font-size: 10px;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+`
 
 class SubMenuItem extends Component {
   render() {
-    const { second_level_link_label, second_level_link1: {url} } = this.props.data
+    let thirdLevelItems = this.props.data.items.map((item, index) => {
+      return (
+          <Link key={index} to='/'>
+           <div style={{color: '#9F090B'}}>🔥 {item._3rd_level_link_text}</div> 
+          </Link>
+
+      )
+    })
+  
+    // const { second_level_link_label, second_level_link1: {url} } = this.props.data
+    console.log(this.props.data.items)
     return (
-      <Link to={url}>
-        <Wrapper>
+      <Wrapper>
           <SecondLevelLink index={this.props.index} length={this.props.length}>
-            {second_level_link_label}
+           {this.props.data.primary.link_text}
+           <ThirdLevelLink>
+             {thirdLevelItems}
+           </ThirdLevelLink>
           </SecondLevelLink>
         </Wrapper>
-      </Link>
     )
   }
 }
